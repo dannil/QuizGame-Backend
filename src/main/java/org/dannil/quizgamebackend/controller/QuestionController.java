57f3b730211ad53a538adcc4fc62044a3abfe0a7
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-12-19 19:35:05 +0100

    Added test data

--- src/main/java/org/dannil/quizgamebackend/controller/QuestionController.java
+++ src/main/java/org/dannil/quizgamebackend/controller/QuestionController.java
@@ -1,5 +1,25 @@
 package org.dannil.quizgamebackend.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/question")
 public final class QuestionController {
 
+	@RequestMapping(method = RequestMethod.GET)
+	public final void questionGET(final HttpServletResponse response) {
+		response.setContentType("application/json;charset=UTF-8");
+		try {
+			response.getWriter().write("{ \"name\" : \"daniel\" }");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
