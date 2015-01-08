40d7126f104bfb6c23f583837b9a894e1a66884f
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-01-08 21:17:12 +0100

    Added logging and removed cast

--- src/main/java/org/dannil/quizgamebackend/interceptor/AuthenticationInterceptor.java
+++ src/main/java/org/dannil/quizgamebackend/interceptor/AuthenticationInterceptor.java
@@ -3,6 +3,7 @@ package org.dannil.quizgamebackend.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.dannil.quizgamebackend.utility.CredentialsUtility;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,9 +12,12 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 @Component
 public final class AuthenticationInterceptor extends HandlerInterceptorAdapter {
 
+	private final static Logger LOGGER = Logger.getLogger(AuthenticationInterceptor.class.getName());
+
 	@Override
 	public final boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) throws Exception {
-		if (!CredentialsUtility.isLoginCorrect((String) request.getParameter("username"), (String) request.getParameter("token"))) {
+		if (!CredentialsUtility.isLoginCorrect(request.getParameter("username"), request.getParameter("token"))) {
+			LOGGER.error("Invalid login, sending 401 response!");
 			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 			return false;
 		}
